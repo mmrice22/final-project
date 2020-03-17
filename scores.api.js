6e7a1c6f1ec0c0706aa98ae8187b8f3a6b7b7835
Roniya Rojan <roniyarojan24@gmail.com>
2020-03-17 14:44:33 -0400

    added post endpoint

--- scores.api.js
+++ scores.api.js
@@ -8,4 +8,15 @@ scoreRoute.get("/scores", (req, res) => {
   });
 });
 
+scoreRoute.post("/scores", (req, res) => {
+  pool
+    .query(
+      "insert into scores (username, city, state, score) values ($1::text, $2::text, $3::text, $4::int)",
+      [req.body.username, req.body.city, req.body.state, req.body.score]
+    )
+    .then(result => {
+      res.send(req.body);
+    });
+});
+
 module.exports = { scoreRoute };
