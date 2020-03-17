2fd5e9e80566e9b8784fa9122f5cd8b494f3ab77
Roniya Rojan <roniyarojan24@gmail.com>
2020-03-17 10:08:42 -0400

    commit

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { questionsRoute } = require("./questions.api");
-<<<<<<< HEAD
-// const {scoreRoute} = require("./scores.api");
-=======
 const { scoreRoute } = require("./scores.api");
->>>>>>> 8836cf92602ee6d9bf68817f22c7541bdf5c3bef
 const app = express();
 app.use(cors());
 app.use(express.json());
