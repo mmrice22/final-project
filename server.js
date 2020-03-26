a2b7365d8075b858f67c859fe842cc3a2d7ae48e
Grant Flennoy <flennoyg@gmail.com>
2020-03-26 14:08:46 -0400

    heroku setup

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/", questionsRoute);
 app.use("/", scoreRoute);
-const port = 3000;
+// define the port
+const DEFAULT_PORT = 3000;
 // Use Heroku's PORT or default to 3000.
-// const port = process.env.PORT || DEFAULT_PORT;
-app.listen(port, () => console.log(`Listening on port: ${port}`));
+const port = process.env.PORT || DEFAULT_PORT;
+// run the server
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
